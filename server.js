268334b73b709e5a79db334f7622cd986db8a2b1
Ubuntu <munir@leaptest.jczj2cgsofwe3e2jbdhrl5hfqc.jx.internal.cloudapp.net>
2016-11-10 17:38:50 +0000

    got mutiple files working!

--- server.js
+++ server.js
@@ -4,9 +4,45 @@ var fs = require('fs');
 var app = express();
 var port = 3000;
 var path = '/home/munir'
-
+var mainlst = []
 
 app.get('/', function(req, res) {
+  var foo = `
+          <html> \
+            <head> \
+              <title> File Directories </title> \
+            </head> \
+            <body> \
+              <h1> File Directory </h1> \
+              <ul>
+  `
+  var baz = `
+	      </ul> \
+              <script src='app.js'></script>
+	    <body> \
+	  </html> 
+  `
+ 
+  var lst = [];
+  fs.readdir(path, function(err, items) {
+    for (var i = 0; i < items.length; i++) {
+      if (items[i].indexOf('.') === -1) {
+        mainlst.push(items[i]);
+        console.log(items[i]);
+        createAppGet(items[i]);
+      }
+      lst.push( '<a href=/' + items[i] + '><li>' + items[i] + '</li></a>');
+      lst.join('');
+    };
+    res.send(([foo, lst.join(), baz]).join('')); 
+  });
+});
+
+
+function createAppGet(newPath) {
+
+app.get('/' + newPath, function(req, res) {
+
   var foo = `
           <html> \
             <head> \
@@ -21,17 +57,22 @@ app.get('/', function(req, res) {
 	    <body> \
 	  </html> 
   `
- 
   var lst = [];
-  fs.readdir(path, function(err, items) {
+  fs.readdir((path + '/' + newPath), function(err, items) {
     for (var i = 0; i < items.length; i++) {
+      if (items[i].indexOf('.') === -1) {
+        console.log(items[i])
+	createAppGet(items[i])    
+      }
       lst.push( '<a href=/' + items[i] + '><li>' + items[i] + '</li></a>');
-      lst.join().replace(',', '');
+      lst.join('');
     };
-    res.send(([foo, lst.join(), baz]).join().replace(/\, /g,"")) 
+    path = path + '/' + newPath;
+    res.send(([foo, lst.join(), baz]).join('')) 
   });
 });
 
+}
 
 app.get('/app.js', function(req,res) {
   res.sendFile('app.js', {root: '.'});
