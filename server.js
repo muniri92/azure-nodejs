2d559af78b71dbd075fbc08fa5da2cf6a296c6e4
muniri92 <mibrah04@gmail.com>
2016-11-13 22:58:39 -0800

    modularized a bit

--- server.js
+++ server.js
@@ -1,131 +1,57 @@
 var express = require('express');
 var fs = require('fs');
-
 var app = express();
 var port = 3000;
-var path = '/Users'
-var mainlst = []
+var path = '/Users';
+var foo = `
+      <html>
+        <head>
+          <title> File Directories </title>
+        </head>
+        <body>
+          <h1> File Directory </h1>
+          <ul>
+`
+var baz = `
+         </ul>
+       <body>
+     </html>
+`
+
 
 app.get('/files', function(req, res) {
-  var foo = `
-          <html> \
-            <head> \
-              <title> File Directories </title> \
-            </head> \
-            <body> \
-              <h1> File Directory </h1> \
-              <ul>
-  `
-  var baz = `
-	      </ul> \
-        <script src="app.js"></script> \
-	    <body> \
-	  </html>
-  `
-
   var lst = [];
   fs.readdir(path, function(err, items) {
     for (var i = 0; i < items.length; i++) {
-      if (items[i].indexOf('.') === -1) {
-        mainlst.push(items[i]);
+      if (fs.lstatSync(path + '/' +  items[i]).isDirectory()) {
         lst.push( '<a href=/files'+ path + '/' +  items[i] + '><li>' + items[i] + '</li></a>');
-        // console.log(items[i]);
-        // createAppGet(items[i]);
       } else {
         lst.push( '<li>' + items[i] + '</li>');
-      }
+      };
       lst.join('');
     };
-    
-    res.send(([foo, lst.join(), baz]).join(''));
+    res.send(([foo, lst.join(''), baz]).join(''));
   });
 });
-
 
 
 app.get('/files/*', function(req, res) {
-  var foo = `
-          <html>
-            <head>
-              <title> File Directories </title>
-            </head>
-            <body>
-              <h1> File Directory </h1>
-              <ul>
-  `
-  var baz = `
-        </ul>
-      <body>
-    </html>
-  `
   var lst = [];
-  var s = req.url 
-  console.log('s: ', s);
-  // var n = s.indexOf('/files/');
-  // console.log(s.split('/files/')[0])
-  s = s.substring(6);
-  console.log('s: ', s);
-  path = s;
-  console.log('path: ', path)
+  var url = req.url;
+  url = url.substring(6);
+  path = url;
   fs.readdir(path, function(err, items) {
-    console.log('ITEMS: ', items)
+    console.log(items)
     for (var i = 0; i < items.length; i++) {
-      if (items[i].indexOf('.') === -1) {
-        mainlst.push(items[i]);
-        lst.push( '<a href=/files' + path + '/' + items[i] + '><li>' + items[i] + '</li></a>');
-        // console.log(items[i]);
-        // createAppGet(items[i]);
+      if (fs.lstatSync(path + '/' +  items[i]).isDirectory()) {
+        lst.push( '<a href=/files'+ path + '/' +  items[i] + '><li>' + items[i] + '</li></a>');
       } else {
         lst.push( '<li>' + items[i] + '</li>');
-      }
+      };
       lst.join('');
     };
-    // path = path + '/' + newPath;
-    res.send(([foo, lst.join(), baz]).join(''))
+    res.send(([foo, lst.join(''), baz]).join(''));
   });
-});
-
-
-
-// function createAppGet(newPath) {
-//   app.get('/' + newPath, function(req, res) {
-//     var foo = `
-//             <html>
-//               <head>
-//                 <title> File Directories </title>
-//               </head>
-//               <body>
-//                 <h1> File Directory </h1>
-//                 <ul>
-//     `
-//     var baz = `
-//   	      </ul>
-//   	    <body>
-//   	  </html>
-//     `
-//     var lst = [];
-//     fs.readdir((path + '/' + newPath), function(err, items) {
-//       for (var i = 0; i < items.length; i++) {
-//         if (items[i].indexOf('.') === -1) {
-//           mainlst.push(items[i]);
-//           lst.push( '<a href=/' + items[i] + '><li>' + items[i] + '</li></a>');
-//           console.log(items[i]);
-//           createAppGet(items[i]);
-//         } else {
-//           lst.push( '<li>' + items[i] + '</li>');
-//         }
-//         lst.join('');
-//       };
-//       path = path + '/' + newPath;
-//       res.send(([foo, lst.join(), baz]).join(''))
-//     });
-//   });
-// };
-
-
-
-app.get('/app.js', function(req,res) {
-  res.sendFile('app.js', {root: '.'});
 });
 
 
